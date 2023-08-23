af9dceac3c22fceea7cdac35b509846b3a22c985
Rob <barrrobert84@gmail.com>
2023-08-23 10:48:38 +0200

    Initial handle submit form without age-badness

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,11 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    console.log(data)
+    console.log()
+})
