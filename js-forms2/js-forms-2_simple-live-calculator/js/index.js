8014276277839b88314b7519c8c57acfa9dc0b87
Rob <barrrobert84@gmail.com>
2023-08-24 11:24:25 +0200

    Simple calculator initial commit

--- js-forms2/js-forms-2_simple-live-calculator/js/index.js
+++ js-forms2/js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,17 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+
+firstInput.addEventListener('input', (e) => {
+    const value1 = firstInput.value
+    const value2 = secondInput.value
+    result.innerHTML = +value1 * +value2
+});
+
+secondInput.addEventListener('input', (e) => {
+    const value1 = firstInput.value
+    const value2 = secondInput.value
+    result.innerHTML = +value1 * +value2
+});
+
