2d472648ac35a9caf7d94c885e2103dc2c9af33f
Rob <barrrobert84@gmail.com>
2023-09-05 10:51:31 +0200

    Futher changes

--- testing/js-unit-testing_get-animal/index.js
+++ testing/js-unit-testing_get-animal/index.js
@@ -1,8 +1,8 @@
 export function getAnimal(animal) {
     if (animal === "cats") {
         return ("I totally love cats!")
-    } else if (animal === "dogs") {
-        return ("I like dogs!")
+    } else if (typeof animal === 'string') {
+        return (`I like ${animal}!`)
     } else {
         if (!animal) {
             return ("I do not like animals at all!")
