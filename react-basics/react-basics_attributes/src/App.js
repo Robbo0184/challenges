5e675294428fedaa1d6a7fd03c0ac7eefc3b3f49
Rob <barrrobert84@gmail.com>
2023-09-11 11:26:14 +0200

    React attributes initial commit

--- react-basics/react-basics_attributes/src/App.js
+++ react-basics/react-basics_attributes/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return john();
+}
+
+function john() {
+  return <article className="article">
+    <h2 className="article__title">Billy Joel is playing live tonight!</h2>
+    <label id="label" htmlFor="input"></label>
+    <input id="input"></input>
+    <a href="https://en.wikipedia.org/wiki/Mallard" className="article__link">Find out why HERE!!!!</a>
+  </article>
 }
