d22d6649292d8447e75faf9c242df32c971012e4
Rob <barrrobert84@gmail.com>
2023-09-11 11:06:55 +0200

    Hello world article

--- react-basics/react-basics_hello-world-article/src/App.js
+++ react-basics/react-basics_hello-world-article/src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./styles.css";
 
+
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return helloWorldArticle()
+}
+
+function helloWorldArticle () {
+  return <article>
+         <h1>The Albatross is extint!</h1>
+         <p>This has been the case for a while.</p>
+        </article>;
 }
