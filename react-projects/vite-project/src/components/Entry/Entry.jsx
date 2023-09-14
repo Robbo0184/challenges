13830eb697c7f0610d89f258c85c5870a80adfa5
Rob <barrrobert84@gmail.com>
2023-09-14 17:11:45 +0200

    More changes such as favourite button implementation.

--- react-projects/vite-project/src/components/Entry/Entry.jsx
+++ react-projects/vite-project/src/components/Entry/Entry.jsx
@@ -1,17 +1,13 @@
-import emptyStarImage from "../../assets/star.svg"
 
-export function Entry({date, headline, text}) {
+import { FavouriteButton } from "../Favourite-Button/Favourite-button"
+
+export function Entry({ date, motto, notes }) {
     return (
-        <section className="entry-list">
         <div className="entry-card">
             <h2 className="date">{date}</h2>
-            <img className="star-image" src={emptyStarImage} />
-            <h2 className="heading">{headline}</h2>
-            <p className="text">{text}</p>
+            <FavouriteButton />
+            <h2 className="heading">{motto}</h2>
+            <p className="text">{notes}</p>
         </div>
-
-     
-    </section>
-
     )
 }
