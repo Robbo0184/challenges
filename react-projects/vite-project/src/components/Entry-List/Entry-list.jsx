c30fd4080d5c17c4ac2f70ec111dc22328b90380
Rob <barrrobert84@gmail.com>
2023-09-14 17:38:52 +0200

    Map of entries array

--- react-projects/vite-project/src/components/Entry-List/Entry-list.jsx
+++ react-projects/vite-project/src/components/Entry-List/Entry-list.jsx
@@ -1,26 +1,46 @@
 import "./Entry-List.css";
 import { Entry } from "../Entry/Entry";
 
+const entries = [
+    {
+      id: 1000,
+      date: "Feb 5, 2025",
+      motto: "We are in a state of chaos",
+      notes:
+        "Today I learned about React State. It was fun! I can't wait to learn more.",
+    },
+    {
+      id: 999,
+      date: "Feb 4, 2025",
+      motto: "Props, Props, Props",
+      notes:
+        "Today I learned about React Props. Mad props to everyone who understands this!",
+    },
+    {
+      id: 998,
+      date: "Feb 3, 2025",
+      motto: "How to nest components online fast",
+      notes:
+        "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+    },
+    {
+      id: 997,
+      date: "Feb 2, 2025",
+      motto: "I'm a React Developer",
+      notes: "My React-ion when I learned about React: 😍",
+    },
+  ];
+
 
 
 export function EntryList() {
+    
     return (
         <section className="entry-list">
             
-        <Entry
-        date= "Jan 06, 2022"
-        motto= "I just want some more cranberries."
-        notes= "Rory lives in Uganda these days, he's writing his thesis." />
-        
-        <Entry
-        date= "Jan 07, 2022"
-        motto= "I just want can't have any more cranberries."
-        notes= "Rory lives in Uganda these days, he's moving to Morocco soon though." />
-
-        <Entry 
-        date= "Jan 08, 2022"
-        motto= "Hi Im Rory."
-        notes= "I just got back from Ireland it was really hot."/>
+       {entries.map((entry) => {
+           return <Entry key={entry.id} date={entry.date} motto={entry.motto} notes={entry.notes}/>
+       })}
             
 
         </section>
--- react-projects/vite-project/src/components/Entry/Entry.jsx
+++ react-projects/vite-project/src/components/Entry/Entry.jsx
@@ -1,9 +1,10 @@
 
 import { FavouriteButton } from "../Favourite-Button/Favourite-button"
 
-export function Entry({ date, motto, notes }) {
+export function Entry({ date, motto, notes, id }) {
     return (
         <div className="entry-card">
+            
             <h2 className="date">{date}</h2>
             <FavouriteButton />
             <h2 className="heading">{motto}</h2>
