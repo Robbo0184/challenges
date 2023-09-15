bf7ab2fd1a066b343fd92a833c8168c4e65def1f
Rob <barrrobert84@gmail.com>
2023-09-15 16:23:27 +0200

    Pokemon with pagination initial commit

--- react-effects-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ react-effects-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [page, setPage] = useState(0)
+ 
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${page * 20}&limit=20`
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -17,12 +19,22 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [page]);
+
+  const goToPreviousPage = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    setPage(page + 1); 
+  };
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={goToPreviousPage}>Previous Page</button>
+      <button type="button" onClick={goToNextPage}>Next Page</button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
