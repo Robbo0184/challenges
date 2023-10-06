987d2d31dcbbc354d15fc4c4cb81c04554438eb5
Rob <barrrobert84@gmail.com>
2023-10-06 15:41:24 +0200

    Random characters

--- backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,20 @@
-export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+import useSWR from "swr";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function Character() {
+  const { data: character, error } = useSWR("/api/random-character", fetcher);
+
+  if (error) {
+    return <p>Error loading character data.</p>;
+  }
+
+  if (!character) {
+    return <p>Loading...</p>;
+  }
+  
+  console.log(character)
+  return <div>Hi {character.firstName} {character.lastName}, you owe us 35000€!!!!!!!!</div>
+
+  
+  
 }
