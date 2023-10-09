c5397b4fb7a1c815bb1ae66deaed63e51057e5f0
Rob <barrrobert84@gmail.com>
2023-10-09 16:29:49 +0200

    Create initial commit

--- backend-read/backend-read_products/pages/api/products/index.js
+++ backend-read/backend-read_products/pages/api/products/index.js
@@ -6,9 +6,20 @@ export default async function handler(request, response) {
   await dbConnect()
 
   if (request.method === "GET") {
-     const products = await Product.find();
-     response.status(200).json(products)
-     
-     
+    const products = await Product.find();
+    response.status(200).json(products)
   }
+
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+      response.status(201).json({ status: "Prodcut created" })
+    }
+    catch (error){
+        console.log(error);
+        response.status(400).json({error: error.message});
+    }
+  }
+
 }
--- backend-read/backend-read_products/pages/index.js
+++ backend-read/backend-read_products/pages/index.js
@@ -1,5 +1,6 @@
 import ProductList from "../components/ProductList";
 import styled from "styled-components";
+import ProductForm from "@/components/ProductForm";
 
 const Heading = styled.h1`
   text-align: center;
@@ -15,7 +16,9 @@ export default function HomePage() {
         </span>
         Fish Shop
       </Heading>
+      
       <ProductList />
+      <ProductForm />
     </>
   );
 }
